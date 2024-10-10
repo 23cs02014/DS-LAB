f1b1fa53dc0cc5a09f5f3763e6340c9029cd6a09
23cs02014 <157111420+23cs02014@users.noreply.github.com>
2024-10-10 14:36:33 +0530

    Add files via upload

---
+++ practice1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+pair<int,int> f(){
+    return {69,420};
+}
+//sorting for pair, first preference first for sorting
+int main()
+{
+map<int,int> f;
+f[10]++;
+
+
+
+
+}
