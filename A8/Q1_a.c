0f51079e559cb3d6d7fc8062e7ab19e899b0ce48
23cs02014 <157111420+23cs02014@users.noreply.github.com>
2024-10-17 11:03:27 +0000

    added 1_a.c

---
+++ A8/Q1_a.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Enter lenght of array:");
+    int n;
+    scanf(" %d",&n);
+    int a[n];
+    printf("Enter values:");
+    for(int i = 0 ; i < n ; i++){
+        scanf(" %d",&a[i]);
+        //printf("%d ",a[i]);
+    }
+    printf("Enter K:");
+    int k;
+    scanf(" %d",&k);
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i + 1 ; j < n ; j++){
+            for(int l = j + 1 ; l < n ; l++){
+                if(a[i]+a[j]+a[l] == k){
+                    printf("%d %d %d \n", a[i],a[j],a[l]);
+                }
+            }
+        }
+    }
+}
