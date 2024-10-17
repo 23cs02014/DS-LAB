91a96d717ba415cb61f88b64b70743132f20fcf3
23cs02014 <157111420+23cs02014@users.noreply.github.com>
2024-10-17 10:55:41 +0000

    doing A8Q1

--- A8/Q1.c
+++ A8/Q1.c
@@ -1,56 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to swap two elements
-void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-// Partition function for quicksort
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-// Quicksort function
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-// Function to print an array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ",arr[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-    int arr[] = {1,2,3};
-    int n = sizeof(arr) / sizeof(arr[0]);
+int main(){
+    printf("Enter lenght of array:");
+    int n;
+    scanf(" %d",&n);
+    int a[n];
+    printf("Enter values:");
     for(int i = 0 ; i < n ; i++){
-        scanf(" %d",&arr[i]);
+        scanf(" %d",&a[i]);
+        printf("%d ",a[i]);
     }
-    printf("Unsorted array: \n");
-    printArray(arr, n);
-    quickSort(arr, 0, n - 1);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
 }
